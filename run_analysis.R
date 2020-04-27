9098f2dbccff95a007ae847a2bbfcd9f235a0518
huyuzhao13 <huyuzhao13@gmail.com>
2020-04-27 00:44:56 -0400

    updated code

--- run_analysis.R
+++ run_analysis.R
@@ -1,60 +1,70 @@
-setwd("D:/Own Files/20200420 all files in getting and cleaning data coursera course")
-train<-read.table("./UCI HAR Dataset/train/X_train.txt")
-test<-read.table("./UCI HAR Dataset/test/X_test.txt")
-features<-read.table("./UCI HAR Dataset/features.txt")
-names<-features[,2]
-names<-as.character(names)
-colnames(train)<-names
-colnames(test)<-names
-merged<-rbind(train,test)
-install.packages("dplyr")
+## Load packages to be used (please install the following packages before load them)
 library(dplyr)
-positions<-grep("mean[(]|std[(]",names)
-selected<-merged[,positions]
+library(mgsub)
+## Download and unzip files
+if ((!file.exists("data.zip"))&(!dir.exists("UCI HAR Dataset"))){
+  download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip","data.zip",mode="wb")
+  unzip("data.zip")
+}
+## Read data and labels
+train_data<-read.table("./UCI HAR Dataset/train/X_train.txt")
+test_data<-read.table("./UCI HAR Dataset/test/X_test.txt")
+features<-read.table("./UCI HAR Dataset/features.txt")
+features<-features[,2]
+features<-as.character(features)
 activity<-read.table("./UCI HAR Dataset/activity_labels.txt")
 activity<-activity[,2]
 activity<-as.character(activity)
-trainlabel<-read.table("./UCI HAR Dataset/train/y_train.txt")
-testlabel<-read.table("./UCI HAR Dataset/test/y_test.txt")
-##trainlabel<-trainlabel[,1]
-##testlabel<-testlabel[,1]
-labelmerge<-rbind(trainlabel,testlabel)
-labelmerge<-labelmerge[,1]
-labelmerge<-as.factor(labelmerge)
-levels(labelmerge)<- c("WALKING","WALKING_UPSTAIRS","WALKING_DOWNSTAIRS","SITTING","STANDING","LAYING")
-final<-cbind(selected,labelmerge)
-colnames(final)[67]<-"activities"
-trainsubject<-read.table("./UCI HAR Dataset/train/subject_train.txt")
-testsubject<-read.table("./UCI HAR Dataset/test/subject_test.txt")
-subjectmerge<-rbind(trainsubject,testsubject)
-colnames(subjectmerge)<-"subject"
-final<-cbind(final,subjectmerge)
-tidydataactivity<-NULL
+train_activity_label<-read.table("./UCI HAR Dataset/train/y_train.txt")
+test_activity_label<-read.table("./UCI HAR Dataset/test/y_test.txt")
+train_subject_number<-read.table("./UCI HAR Dataset/train/subject_train.txt")
+test_subject_number<-read.table("./UCI HAR Dataset/test/subject_test.txt")
+## Merge train and test data set
+colnames(train_data)<-features
+colnames(test_data)<-features
+merged_data<-rbind(train_data,test_data)
+## Extracts only the measurements on the mean and standard deviation for each measurement
+mean_std_positions<-grep("mean[(]|std[(]",features)
+merged_mean_std_data<-merged_data[,mean_std_positions]
+## Uses descriptive activity names to name the activities in the data set
+activity_label_merge<-rbind(train_activity_label,test_activity_label)
+activity_label_merge<-activity_label_merge[,1]
+activity_label_merge<-as.factor(activity_label_merge)
+levels(activity_label_merge)<- activity
+data_with_activity<-cbind(merged_mean_std_data,activity_label_merge)
+colnames(data_with_activity)[67]<-"activities"
+subject_number_merge<-rbind(train_subject_number,test_subject_number)
+colnames(subject_number_merge)<-"subject"
+data_with_activity_and_subject<-cbind(data_with_activity,subject_number_merge)
+##Appropriately labels the data set with descriptive variable names
+colnames(data_with_activity_and_subject)<-mgsub(colnames(data_with_activity_and_subject),c("^t","^f","Acc","[(][)]","X","std","Y","Z","Mag","Gyro","-","BodyBody","mean"),c("Time","Frequency","Accelerometer","","X_axis","Standard_deviation","Y_axis","Z_axis","Magnitude","Gyroscope","","Body","Mean"))
+## From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject
+tidy_data_activity<-NULL
 for (i in 1:66){
   if(i==1){
-    t<-tapply(final[,i],final$activities,mean)
+    t<-tapply(data_with_activity_and_subject[,i],data_with_activity_and_subject$activities,mean)
     t<-as.data.frame(t)
-    colnames(t)<-colnames(final)[i]
-    tidydataactivity<-t
+    colnames(t)<-colnames(data_with_activity_and_subject)[i]
+    tidy_data_activity<-t
   } else {
-    t<-tapply(final[,i],final$activities,mean)
+    t<-tapply(data_with_activity_and_subject[,i],data_with_activity_and_subject$activities,mean)
     t<-as.data.frame(t)
-    colnames(t)<-colnames(final)[i]
-    tidydataactivity<-cbind(tidydataactivity,t)
+    colnames(t)<-colnames(data_with_activity_and_subject)[i]
+    tidy_data_activity<-cbind(tidy_data_activity,t)
   }
 }
-tidydatasubject<-NULL
+tidy_data_subject<-NULL
 for (i in 1:66){
   if(i==1){
-    t<-tapply(final[,i],final$subject,mean)
+    t<-tapply(data_with_activity_and_subject[,i],data_with_activity_and_subject$subject,mean)
     t<-as.data.frame(t)
-    colnames(t)<-colnames(final)[i]
-    tidydatasubject<-t
+    colnames(t)<-colnames(data_with_activity_and_subject)[i]
+    tidy_data_subject<-t
   } else {
-    t<-tapply(final[,i],final$subject,mean)
+    t<-tapply(data_with_activity_and_subject[,i],data_with_activity_and_subject$subject,mean)
     t<-as.data.frame(t)
-    colnames(t)<-colnames(final)[i]
-    tidydatasubject<-cbind(tidydatasubject,t)
+    colnames(t)<-colnames(data_with_activity_and_subject)[i]
+    tidy_data_subject<-cbind(tidy_data_subject,t)
   }
 }
-tidydata<-rbind(tidydataactivity,tidydatasubject)
+tidydata<-rbind(tidy_data_activity,tidy_data_subject)
